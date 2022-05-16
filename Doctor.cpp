0fd49277f2c102832b8c253ddfd6c261ad9dc8c5
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-16 17:39:08 +0530

    Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -9,9 +9,25 @@ Doctor :: Doctor() {
   Email = "";
   ContactNo = "";
 }
+
 Doctor :: Doctor(string name, string email, string contactNo, string doctorId) {
   DoctorId = doctorId;
   Name = name;
   Email = email;
   ContactNo = contactNo;
+}
+
+void Doctor :: Display() {
+  cout << "This is Doctor Class >> Derived from Person\n\n";
+}
+
+void Doctor :: DisplayDoctorData() {
+	cout << "Doctor Id	: " << DoctorId << "\n";
+	cout << "Name		: " << Name  << "\n";
+	cout << "Email		: " << Email << "\n";
+	cout << "Cotact Number	: " << ContactNo << "\n\n";
+}
+
+Doctor :: ~Doctor() {
+	cout << "Destructor Called\n";
 }
