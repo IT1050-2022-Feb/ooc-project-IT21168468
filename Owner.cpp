ccacb945a5e162a6ae665deab2ed7d4ff79bc704
Nipuni Dhananjana <99585594+IT21168468@users.noreply.github.com>
2022-05-16 17:39:54 +0530

    Owner.cpp

--- Owner.cpp
+++ Owner.cpp
@@ -14,3 +14,18 @@ Owner :: Owner(string name, string email, string contactNo) {
 	Email = email;
 	ContactNo = contactNo;
 }
+
+
+void Owner :: Display() {
+  cout << "This is Owner Class >> Derived from Person\n\n";
+}
+
+void Owner :: DisplayOwnerData() {
+	cout << "Name		: " << Name  << "\n";
+	cout << "Email		: " << Email << "\n";
+	cout << "Cotact Number	: " << ContactNo << "\n\n";
+}
+
+Owner :: ~Owner() {
+	cout << "Destructor Called\n";
+}
